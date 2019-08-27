e6535c37c5fbd806b66a3147e568776fb6322a39
Tim Riffe <tim.riffe@gmail.com>
2019-08-27 08:45:26 -0400

    Initial commit

---
+++ setup.R
@@ -0,0 +1,9 @@
+#
+library(usethis)
+?browse_github()
+use_git_config(user.name = "Tim Riffe", user.email = "tim.riffe@gmail.com")
+
+# do only once:
+# browse_github_token()
+# GITHUB_PAT=36c530f1e959cf6fc980ccfdc571d8324fe8971f 
+# edit_r_environ()
