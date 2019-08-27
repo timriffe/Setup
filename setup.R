851ae5855307f5c4654f337ee607bdef51097aa9
Tim Riffe <tim.riffe@gmail.com>
2019-08-27 08:54:26 -0400

    .

--- setup.R
+++ setup.R
@@ -5,7 +5,7 @@ use_git_config(user.name = "Tim Riffe", user.email = "tim.riffe@gmail.com")
 
 # do only once:
 # browse_github_token()
-# GITHUB_PAT=a6ce7ca27ec8178fc9fe1b00d7d1da47608a787e
+
 # edit_r_environ()
 use_git()
 use_github()
